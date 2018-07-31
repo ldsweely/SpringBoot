1be713732925b026da62417ebdf6dfc8b18d46bb
liuds <ldsweely@hotmail.com>
2018-07-31 14:15:27 +0800

    f

---
+++ springboot210/src/main/java/com/example/springboot210/Springboot210Application.java
@@ -0,0 +1,12 @@
+package com.example.springboot210;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot210Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot210Application.class, args);
+	}
+}
